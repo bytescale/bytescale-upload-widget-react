21879d22e6f3f027a9f3b736ea9863d8b6aab9d3
Lawrence Wagerfield <lawrence@projectone.io>
2023-09-18 14:49:45 +0100

    Release 4.7.0

--- src/UploadDropzone.tsx
+++ src/UploadDropzone.tsx
@@ -1,8 +1,12 @@
 import React, { useLayoutEffect } from "react";
 import { useElementRef } from "./hooks/UseElementRef";
 import { useAutoUpdatingOptions } from "@bytescale/upload-widget-react/hooks/UseAutoUpdatingOptions";
-import { UploadWidget, UploadWidgetConfig, UploadWidgetResult } from "@bytescale/upload-widget";
-import { UploadWidgetOnUpdateEvent } from "@bytescale/upload-widget/dist/config/UploadWidgetOnUpdateEvent";
+import {
+  UploadWidget,
+  UploadWidgetConfig,
+  UploadWidgetResult,
+  UploadWidgetOnUpdateEvent
+} from "@bytescale/upload-widget";
 
 interface Props {
   className?: string;
